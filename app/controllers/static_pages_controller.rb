e2721f78633b3b4e5194c3fd71731eb50dfaa2eb
Dani Cela <dani@Danis-MacBook-Pro.local>
2012-06-05 10:36:42 -0400

    Finish Static Pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,8 @@ class StaticPagesController < ApplicationController
 
   def help
   end
+  
+  def about
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SampleApp::Application.routes.draw do
   get "static_pages/home"
-
   get "static_pages/help"
+  get "static_pages/about"
 
 end
